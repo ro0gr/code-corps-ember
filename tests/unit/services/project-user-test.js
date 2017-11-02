02ee19814842fdad52564c8af9ff906c65c30f39
Dave Connis <dave@codecorps.org>
2017-11-02 15:53:17 -0400

    started writing unit test

--- tests/unit/services/project-user-test.js
+++ tests/unit/services/project-user-test.js
@@ -1,14 +1,47 @@
+import RSVP from 'rsvp';
+import { set, get } from '@ember/object';
 import { moduleFor, test } from 'ember-qunit';
 
+let mockStore = {
+  createRecord() {
+    return {
+      save() {
+        return RSVP.resolve({ created: true });
+      }
+    };
+  }
+};
+
+let currentUser = get(this, 'currentUser.user');
+
+let projectUser = {
+  currentUser,
+  project: 'Code Corps',
+  role:'pending'
+};
+
 moduleFor('service:project-user', 'Unit | Service | project user', {
-  // Specify the other units that are required for this test.
-  needs: [
-    'service:current-user',
-    'service:flash-messages'
-  ]
+  needs: ['service:current-user', 'service:flash-messages'],
+  beforeEach() {
+    let service = this.subject();
+    set(service, 'store', mockStore);
+  }
 });
 
-// Replace this with your real tests.
+test('it creates a new projectUser with properties', function(assert) {
+  let done = assert.async();
+  let service = this.subject();
+  service.joinProject()
+
+
+  assert.equal(get(service, 'projectUser'))
+
+});
+
+test('it creates a flash notification on success', function(assert) {
+let service = this.subject();
+});
+
 test('it exists', function(assert) {
   let service = this.subject();
   assert.ok(service);
